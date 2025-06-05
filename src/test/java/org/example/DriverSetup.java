58a2166f1f64175d131ebaacbc586fd0e1690186
samirasidd <samira.siddiqua@gmail.com>
2025-06-05 23:43:24 +0600

    Updated wait

--- src/test/java/org/example/DriverSetup.java
+++ src/test/java/org/example/DriverSetup.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class DriverSetup {
     public WebDriver driver;
 
     @BeforeSuite
     public void startBrowser(){
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
     }
 
 
